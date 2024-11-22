40ee920187956c64ecf13c1fa6c2fd5924dad981
Jaesel Pontilla <159694834+PontillaJaesel@users.noreply.github.com>
2024-11-22 15:54:42 +0800

    Update Dancer.java

--- Laboratory Activities/Laboratoy Activity 3 - 22 - November - 2024/Artists/Dancer.java
+++ Laboratory Activities/Laboratoy Activity 3 - 22 - November - 2024/Artists/Dancer.java
@@ -23,7 +23,7 @@ public class Dancer extends Artist {
 
     @Override
     public void displayInfo() {
-        System.out.println("== Dancer Info ==");
+        System.out.println("Dancer Info: ");
         super.displayInfo();
         System.out.println("Dancer: " + dance_style);
     }
