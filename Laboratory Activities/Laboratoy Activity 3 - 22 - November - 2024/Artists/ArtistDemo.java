a7c67b873d3c4f1e78fea1dc3d29502eee410303
Jaesel Pontilla <159694834+PontillaJaesel@users.noreply.github.com>
2024-11-22 16:16:03 +0800

    Update ArtistDemo.java

--- Laboratory Activities/Laboratoy Activity 3 - 22 - November - 2024/Artists/ArtistDemo.java
+++ Laboratory Activities/Laboratoy Activity 3 - 22 - November - 2024/Artists/ArtistDemo.java
@@ -1,27 +1,27 @@
 public class ArtistDemo {
     public static void main(String[] args) {
         // instance of Artist
-        Artist artist = new Artist("Charlie Puth", "American", 32, "Music");
+        Artist artist = new Artist("Charlie Puth", 32, "Music");
         artist.displayInfo();
 
         System.out.println();
 
-        Singer singer = new Singer("Nayeon Im", "Korean", 29, "Music", Genre.POP);
+        Singer singer = new Singer("Nayeon Im", 29, "Music", Genre.POP);
         singer.displayInfo();
 
         System.out.println();
 
-        Dancer dancer = new Dancer("Kwon Hoshi", "Korean", 28, "Dancing", DanceStyle.HIPHOP);
+        Dancer dancer = new Dancer("Kwon Hoshi", 28, "Dancing", DanceStyle.HIPHOP);
         dancer.displayInfo();
 
         System.out.println();
 
-        Painter painter = new Painter("Bob Ross", "American", 52, "Painting", Medium.OIL);
+        Painter painter = new Painter("Bob Ross", 52, "Painting", Medium.OIL);
         painter.displayInfo();
 
         System.out.println();
         
-        Writer writer = new Writer("J. K. Rowling", "British", 59, "Writing", WritingStyle.FICTION);
+        Writer writer = new Writer("J. K. Rowling", 59, "Writing", WritingStyle.FICTION);
         writer.displayInfo();
 
         System.out.println();
