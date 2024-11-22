92bd1e48cb649b37db8de65375a220e823e7347a
Jaesel Pontilla <159694834+PontillaJaesel@users.noreply.github.com>
2024-11-22 15:42:34 +0800

    Create Dancer.java

---
+++ Laboratory Activities/Laboratoy Activity 3 - 22 - November - 2024/Artists/Dancer.java
@@ -0,0 +1,31 @@
+enum DanceStyle {
+    BALLET,
+    HIPHOP,
+    JAZZ,
+    CONTEMPORARY
+}
+
+public class Dancer extends Artist {
+    private DanceStyle dance_style;
+
+    public Dancer(String name, String nationality, int age, String specialty, DanceStyle dancestyle) {
+        super(name, nationality, age, specialty);
+        this.dance_style = dancestyle;
+    }
+
+    public DanceStyle getDanceStyle() {
+        return dance_style;
+    }
+
+    public void setDanceStyle(DanceStyle dancestyle) {
+        this.dance_style = dancestyle;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("== Dancer Info ==");
+        super.displayInfo();
+        System.out.println("Dancer: " + dance_style);
+    }
+    
+}
