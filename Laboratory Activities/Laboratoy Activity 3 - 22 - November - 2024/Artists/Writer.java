e99763366af1e1a0503adab4b4fc9297eebfe2f2
Jaesel Pontilla <159694834+PontillaJaesel@users.noreply.github.com>
2024-11-22 15:45:04 +0800

    Create Writer.java

---
+++ Laboratory Activities/Laboratoy Activity 3 - 22 - November - 2024/Artists/Writer.java
@@ -0,0 +1,31 @@
+enum WritingStyle{
+    FICTION,
+    NONFICTION,
+    POETRY,
+    DRAMA,
+}
+                
+
+public class Writer extends Artist {
+    private WritingStyle writing_style;
+
+    public Writer(String name, String nationality, int age, String specialty, WritingStyle writingstyle) {
+        super(name, nationality, age, specialty);
+        this.writing_style = writingstyle;
+    }
+
+    public WritingStyle getWritingStyle() {
+        return writing_style;
+    }
+
+    public void setWritingStyle(WritingStyle writingstyle) {
+        this.writing_style = writingstyle;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("== Writer Info ==");
+        super.displayInfo();
+        System.out.println("Writing Style: " + writing_style);
+    }
+}
