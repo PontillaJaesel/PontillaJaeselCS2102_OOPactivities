c5816648be54648601e13c499bde5fede8e9100f
Jaesel Pontilla <159694834+PontillaJaesel@users.noreply.github.com>
2024-11-29 20:10:12 +0800

    Update Triangle.java

--- Laboratory Activities/Laboratory Activity 4 - 29 - November -2024/Triangle.java
+++ Laboratory Activities/Laboratory Activity 4 - 29 - November -2024/Triangle.java
@@ -1,1 +1,26 @@
+public class Triangle implements Shape{
+    private double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double getArea() {
+        double s = (a + b + c) / 2;
+        double area = Math.sqrt(s * (s-a) * (s-b) * (s-c));
+        return area;
+    }
+
+    public double getPerimeter() {
+        double perimeter = a + b + c;
+        return perimeter;
+    }
+
+    public void printShapeType() {
+        System.out.println("Triangle");
+    }
+}
 
