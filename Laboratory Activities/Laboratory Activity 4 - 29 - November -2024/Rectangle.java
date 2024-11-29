f6a92e2157e54197df054dd0790c0636d95e2b10
Jaesel Pontilla <159694834+PontillaJaesel@users.noreply.github.com>
2024-11-29 20:09:02 +0800

    Update Rectangle.java

--- Laboratory Activities/Laboratory Activity 4 - 29 - November -2024/Rectangle.java
+++ Laboratory Activities/Laboratory Activity 4 - 29 - November -2024/Rectangle.java
@@ -1,1 +1,24 @@
+public class Rectangle implements Shape{
+    private double length;
+    private double width;
 
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        double area = length * width;
+        return area;
+    }
+
+    public double getPerimeter() {
+        double perimeter = (2 * length) + (2 * width);
+        return perimeter;
+    }
+    
+    public void printShapeType() {
+        System.out.println("Rectangle");
+    }
+}
